de0f0986a48c7fefb5729b85fd0394a42dc13445
Anthony H <ahand@users.noreply.github.com>
2021-04-20 16:58:10 -0700

    UXPin Config
    Adding ActionButton

--- uxpin.config.js
+++ uxpin.config.js
@@ -5,6 +5,7 @@ module.exports = {
         name: 'General',
         include: [
           'src/components/Button/Button.js',
+          'src/components/ActionButton/ActionButton.js',
         ],
       },
     ],
