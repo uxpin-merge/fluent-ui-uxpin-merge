8de48f3010f0e3b180c9a82b1d7e2c3c7146bead
Anthony H <ahand@users.noreply.github.com>
2021-04-21 09:52:17 -0700

    Update Link.js

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import { Link as FLink } from '@fluentui/react/lib/Link';
-import { Text as FText } from '@fluentui/react/lib/Text';
+import { Text } from '@fluentui/react/lib/Text';
 
 
 
