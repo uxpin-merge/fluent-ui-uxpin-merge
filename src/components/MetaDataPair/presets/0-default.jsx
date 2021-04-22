29f7be8e9d8c383c10bf1665f7cf80f0ef024894
Anthony H <ahand@users.noreply.github.com>
2021-04-22 15:45:43 -0700

    Update 0-default.jsx

--- src/components/MetaDataPair/presets/0-default.jsx
+++ src/components/MetaDataPair/presets/0-default.jsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import MetaDataPair from '../MetaDataPair';
+import Text from '../Text';
 
 
 
 export default (
-    <MetaDataPair uxpId="metadatapair1" />
+    <MetaDataPair uxpId="metadatapair1" >
+        <Text uxpID="text1" value="Right side text object. Expand the MetaDataPair object in the Layers panel, and click to change the text or delete." />
+    </MetaDataPair>
 );
