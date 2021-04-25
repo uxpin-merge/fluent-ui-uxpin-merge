9f8fff2b82de96d137ff7ef420968b02d3681b99
Anthony H <ahand@users.noreply.github.com>
2021-04-25 14:50:28 -0700

    CardBody
    Adding Text instructions to the body

--- src/components/CardBody/presets/0-default.jsx
+++ src/components/CardBody/presets/0-default.jsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import CardBody from '../CardBody';
+import Text from '../../Text/Text';
 
 
+
+const instructions = 'Add any Merge control to the CardBody';
 
 export default (
-    <CardBody uxpId="cardbody1" />
+    <CardBody uxpId="cardbody1">
+        <Text uxpId="text1" value={instructions} />
+    </CardBody>
 );
