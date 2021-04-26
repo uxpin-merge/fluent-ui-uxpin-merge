f7c1fcc6e3b4927295feb2ac5733e9d2284f0290
Anthony H <ahand@users.noreply.github.com>
2021-04-26 12:00:35 -0700

    Cardfooter
    added ToggleButton

--- src/components/CardFooter/presets/0-default.jsx
+++ src/components/CardFooter/presets/0-default.jsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import CardFooter from '../CardFooter';
+import ToggleButton from '../../ToggleButton/ToggleButton';
 import ActionButton from '../../ActionButton/ActionButton';
+
 
 
 
@@ -9,7 +11,8 @@ const iconName = "Eye";
 
 
 export default (
-    <CardFooter uxpId="cardfooter1" addSpanner={true} spannerIndex={1} >
+    <CardFooter uxpId="cardfooter1" addSpanner={true} spannerIndex={2} >
+        <ToggleButton uxpId="togglebutton1" />
         <ActionButton uxpId="actionbutton1" text={text} iconName={iconName} />
     </CardFooter>
 );
