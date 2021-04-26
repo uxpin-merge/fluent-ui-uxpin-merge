236dcc153ada1b13336a2b778a39bbf105416d09
Anthony H <ahand@users.noreply.github.com>
2021-04-26 15:46:38 -0700

    Update 0-default.jsx
    Pre-populating the Card

--- src/components/Card/presets/0-default.jsx
+++ src/components/Card/presets/0-default.jsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import Card from '../Card';
+import CardHeader from '../../CardHeader';
+import CardBody from '../../CardBody';
+import CardFooter from '../../CardFooter';
 
 
 
 export default (
-    <Card uxpId="card1" />
+    <Card uxpId="card1">
+        <CardHeader uxpId="cardheader1" />
+        <CardBody uxpId="cardbody1" />
+        <CardFooter uxpId="cardfooter1" />
+    </Card>
 );
