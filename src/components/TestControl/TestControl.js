e7af6e4a065b71bd6525c60543258954d28e3db0
Anthony H <ahand@users.noreply.github.com>
2021-05-09 18:38:53 -0700

    Update TestControl.js

--- src/components/TestControl/TestControl.js
+++ src/components/TestControl/TestControl.js
@@ -46,22 +46,22 @@ class TestControl extends React.Component {
         if (df) {
             for (var key in df) {
                 if (df.hasOwnProperty(key)) {
-                    console.log("Semantic Color key + ": " + df[key]);
+                    console.log("Semantic Color " + key + ": " + df[key]);
                 }
             };
         }
 
-        // let themeDark = UxpColors.getHexFromFluentToken("themeDark");
-        // console.log("**** themeDark: " + themeDark);
+        let themeDark = UxpColors.getHexFromFluentToken("themeDark");
+        console.log("**** themeDark: " + themeDark);
 
-        // let gray220 = UxpColors.getHexFromFluentToken("gray220");
-        // console.log("**** Neutral gray220: " + gray220);
+        let gray220 = UxpColors.getHexFromFluentToken("gray220");
+        console.log("**** Neutral gray220: " + gray220);
 
-        // let redOrange20 = UxpColors.getHexFromFluentToken("redOrange20");
-        // console.log("**** Shared redOrange20: " + redOrange20);
+        let redOrange20 = UxpColors.getHexFromFluentToken("redOrange20");
+        console.log("**** Shared redOrange20: " + redOrange20);
 
-        // let primary = UxpColors.getHexFromFluentToken("primary");
-        // console.log("**** Comms primary: " + primary);
+        let primary = UxpColors.getHexFromFluentToken("primary");
+        console.log("**** Comms primary: " + primary);
 
         this.state = {
         }
