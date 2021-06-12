764675b53f66802b02ecb72c515b9728d6c8d47f
Anthony H <ahand@users.noreply.github.com>
2021-06-11 18:26:04 -0700

    Update uxpcolorutils.js
    Removing PayPal color tokens

--- src/components/_helpers/uxpcolorutils.js
+++ src/components/_helpers/uxpcolorutils.js
@@ -2,61 +2,53 @@ import { DefaultPalette, CommunicationColors, NeutralColors, SharedColors } from
 
 
 
-
-//TODO: Leverage this technique
-// import { getTheme } from '@fluentui/react/lib/Styling';
-// const theme = getTheme();
-//console.log("Theme info: theme.palette.themeLighterAlt - " + theme.palette.themeLighterAlt);
-
-
-
 export const UxpColors = {
 
     textColor: '#000000',
     pageColor: '#ffffff',
 
-    //******  Greys  ******  
-    black: '#000000',
-    white: '#ffffff',
-    grey700: '#2C2E2F',
-    grey600: '#687173',
-    grey500: '#9DA3A6',
-    grey400: '#B7BCBF',
-    grey300: '#CBD2D6',
-    grey200: '#EAECED',
-    grey100: '#F5F7FA',
+    // //******  Greys  ******  
+    // black: '#000000',
+    // white: '#ffffff',
+    // grey700: '#2C2E2F',
+    // grey600: '#687173',
+    // grey500: '#9DA3A6',
+    // grey400: '#B7BCBF',
+    // grey300: '#CBD2D6',
+    // grey200: '#EAECED',
+    // grey100: '#F5F7FA',
 
-    //******  Blues ******
-    blue800: '#003087',
-    blue700: '#005EA6',
-    blue600: '#0070BA',
-    blue500: '#009CDE',
-    blue400: '#50C7F9',
-    blue300: '#BFDBEE',
-    blue200: '#E4F1FB',
-    blue100: '#F5FBFE',
+    // //******  Blues ******
+    // blue800: '#003087',
+    // blue700: '#005EA6',
+    // blue600: '#0070BA',
+    // blue500: '#009CDE',
+    // blue400: '#50C7F9',
+    // blue300: '#BFDBEE',
+    // blue200: '#E4F1FB',
+    // blue100: '#F5FBFE',
 
-    //******  Accents  ******  
+    // //******  Accents  ******  
 
-    //Greens
-    green700: '#018065',
-    green600: '#299976',
-    green500: '#00CF92',
-    green100: '#F5FDFB',
+    // //Greens
+    // green700: '#018065',
+    // green600: '#299976',
+    // green500: '#00CF92',
+    // green100: '#F5FDFB',
 
-    //Oranges
-    orange700: '#D64003',
-    orange500: '#FF9600',
-    orange300: '#FFBD5D',
-    orange100: '#FFFBF5',
+    // //Oranges
+    // orange700: '#D64003',
+    // orange500: '#FF9600',
+    // orange300: '#FFBD5D',
+    // orange100: '#FFFBF5',
 
-    //Reds
-    red500: '#D20000',
-    red100: '#FFF7F7',
+    // //Reds
+    // red500: '#D20000',
+    // red100: '#FFF7F7',
 
-    //Other
-    purple500: '#640487',
-    pink500: '#DE0063',
+    // //Other
+    // purple500: '#640487',
+    // pink500: '#DE0063',
 
     //******  Semantic Colors  ****** 
     // A few key semantic colors
@@ -113,73 +105,73 @@ export const UxpColors = {
         return params;
     },
 
-    /**
-     * A convenience method for getting an array of color params for the  Blue palette.
-     * Each param has an id (the short token name), a token-like name, the actual token name, and a hex value of the color. 
-     * This array is useful for creating Microsoft Fluent Color objects. 
-     * @returns {Array} Returns an array of Color Params objects with the  Blues palette. Attributes are: id, label, colorToken, and color (a Hex value).
-     */
-    getBlueColorList: function () {
-        let b8 = this.getColorParams("blue-800", this.blue800);
-        let b7 = this.getColorParams("blue-700", this.blue700);
-        let b6 = this.getColorParams("blue-600", this.blue600);
-        let b5 = this.getColorParams("blue-500", this.blue500);
-        let b4 = this.getColorParams("blue-400", this.blue400);
-        let b3 = this.getColorParams("blue-300", this.blue300);
-        let b2 = this.getColorParams("blue-200", this.blue200);
-        let b1 = this.getColorParams("blue-100", this.blue100);
+    // /**
+    //  * A convenience method for getting an array of color params for the  Blue palette.
+    //  * Each param has an id (the short token name), a token-like name, the actual token name, and a hex value of the color. 
+    //  * This array is useful for creating Microsoft Fluent Color objects. 
+    //  * @returns {Array} Returns an array of Color Params objects with the  Blues palette. Attributes are: id, label, colorToken, and color (a Hex value).
+    //  */
+    // getBlueColorList: function () {
+    //     let b8 = this.getColorParams("blue-800", this.blue800);
+    //     let b7 = this.getColorParams("blue-700", this.blue700);
+    //     let b6 = this.getColorParams("blue-600", this.blue600);
+    //     let b5 = this.getColorParams("blue-500", this.blue500);
+    //     let b4 = this.getColorParams("blue-400", this.blue400);
+    //     let b3 = this.getColorParams("blue-300", this.blue300);
+    //     let b2 = this.getColorParams("blue-200", this.blue200);
+    //     let b1 = this.getColorParams("blue-100", this.blue100);
 
-        let palette = [b8, b7, b6, b5, b4, b3, b2, b1];
-        return palette;
-    },
+    //     let palette = [b8, b7, b6, b5, b4, b3, b2, b1];
+    //     return palette;
+    // },
 
-    /**
-     * A convenience method for getting an array of color params for the  Grey palette.
-     * Each param has an id (the short token name), a token-like name, the actual token name, and a hex value of the color. 
-     * This array is useful for creating Microsoft Fluent Color objects. 
-     * @returns {Array} Returns an array of Color Params objects with the Greys palette. Attributes are: id, label, colorToken, and color (a Hex value).
-     */
-    getGreyColorList: function () {
-        let b9 = this.getColorParams("black", this.black);
-        let b8 = this.getColorParams("white", this.white);
-        let b7 = this.getColorParams("grey-700", this.grey700);
-        let b6 = this.getColorParams("grey-600", this.grey600);
-        let b5 = this.getColorParams("grey-500", this.grey500);
-        let b4 = this.getColorParams("grey-400", this.grey400);
-        let b3 = this.getColorParams("grey-300", this.grey300);
-        let b2 = this.getColorParams("grey-200", this.grey200);
-        let b1 = this.getColorParams("grey-100", this.grey100);
+    // /**
+    //  * A convenience method for getting an array of color params for the  Grey palette.
+    //  * Each param has an id (the short token name), a token-like name, the actual token name, and a hex value of the color. 
+    //  * This array is useful for creating Microsoft Fluent Color objects. 
+    //  * @returns {Array} Returns an array of Color Params objects with the Greys palette. Attributes are: id, label, colorToken, and color (a Hex value).
+    //  */
+    // getGreyColorList: function () {
+    //     let b9 = this.getColorParams("black", this.black);
+    //     let b8 = this.getColorParams("white", this.white);
+    //     let b7 = this.getColorParams("grey-700", this.grey700);
+    //     let b6 = this.getColorParams("grey-600", this.grey600);
+    //     let b5 = this.getColorParams("grey-500", this.grey500);
+    //     let b4 = this.getColorParams("grey-400", this.grey400);
+    //     let b3 = this.getColorParams("grey-300", this.grey300);
+    //     let b2 = this.getColorParams("grey-200", this.grey200);
+    //     let b1 = this.getColorParams("grey-100", this.grey100);
 
-        let palette = [b9, b8, b7, b6, b5, b4, b3, b2, b1];
-        return palette;
-    },
+    //     let palette = [b9, b8, b7, b6, b5, b4, b3, b2, b1];
+    //     return palette;
+    // },
 
-    /**
-     * A convenience method for getting an array of color params for the  Accent palette.
-     * Each param has an id (the short token name), a token-like name, the actual token name, and a hex value of the color. 
-     * This array is useful for creating Microsoft Fluent Color objects. 
-     * @returns {Array} Returns an array of Color Params objects with the Accents palette. Attributes are: id, label, colorToken, and color (a Hex value).
-     */
-    getAccentColorList: function () {
-        let b12 = this.getColorParams("green-700", this.green700);
-        let b11 = this.getColorParams("green-600", this.green600);
-        let b10 = this.getColorParams("green-500", this.green500);
-        let b9 = this.getColorParams("green-100", this.green100);
+    // /**
+    //  * A convenience method for getting an array of color params for the  Accent palette.
+    //  * Each param has an id (the short token name), a token-like name, the actual token name, and a hex value of the color. 
+    //  * This array is useful for creating Microsoft Fluent Color objects. 
+    //  * @returns {Array} Returns an array of Color Params objects with the Accents palette. Attributes are: id, label, colorToken, and color (a Hex value).
+    //  */
+    // getAccentColorList: function () {
+    //     let b12 = this.getColorParams("green-700", this.green700);
+    //     let b11 = this.getColorParams("green-600", this.green600);
+    //     let b10 = this.getColorParams("green-500", this.green500);
+    //     let b9 = this.getColorParams("green-100", this.green100);
 
-        let b8 = this.getColorParams("orange-700", this.orange700);
-        let b7 = this.getColorParams("orange-500", this.orange500);
-        let b6 = this.getColorParams("orange-300", this.orange300);
-        let b5 = this.getColorParams("orange-100", this.orange100);
+    //     let b8 = this.getColorParams("orange-700", this.orange700);
+    //     let b7 = this.getColorParams("orange-500", this.orange500);
+    //     let b6 = this.getColorParams("orange-300", this.orange300);
+    //     let b5 = this.getColorParams("orange-100", this.orange100);
 
-        let b4 = this.getColorParams("red-500", this.red500);
-        let b3 = this.getColorParams("red-100", this.red100);
+    //     let b4 = this.getColorParams("red-500", this.red500);
+    //     let b3 = this.getColorParams("red-100", this.red100);
 
-        let b2 = this.getColorParams("purples-500", this.purple500);
-        let b1 = this.getColorParams("pink-500", this.pink500);
+    //     let b2 = this.getColorParams("purples-500", this.purple500);
+    //     let b1 = this.getColorParams("pink-500", this.pink500);
 
-        let palette = [b12, b11, b10, b9, b8, b7, b6, b5, b4, b3, b2, b1];
-        return palette;
-    },
+    //     let palette = [b12, b11, b10, b9, b8, b7, b6, b5, b4, b3, b2, b1];
+    //     return palette;
+    // },
 
 
     /**
@@ -194,75 +186,75 @@ export const UxpColors = {
         let t = token.trim().toLowerCase().replace("-", "").replace(" ", "").replace("gray", "grey");
 
         switch (t) {
-            case "blue800":
-                return this.blue800;
-            case "blue700":
-                return this.blue700;
-            case "blue600":
-                return this.blue600;
-            case "blue500":
-                return this.blue500;
-            case "blue400":
-                return this.blue400;
-            case "blue300":
-                return this.blue300;
-            case "blue200":
-                return this.blue200;
-            case "blue100":
-                return this.blue100;
+            // case "blue800":
+            //     return this.blue800;
+            // case "blue700":
+            //     return this.blue700;
+            // case "blue600":
+            //     return this.blue600;
+            // case "blue500":
+            //     return this.blue500;
+            // case "blue400":
+            //     return this.blue400;
+            // case "blue300":
+            //     return this.blue300;
+            // case "blue200":
+            //     return this.blue200;
+            // case "blue100":
+            //     return this.blue100;
 
-            //Greys
-            case "black":
-                return this.black;
-            case "white":
-                return this.white;
-            case "grey700":
-                return this.grey700;
-            case "grey600":
-                return this.grey600;
-            case "grey500":
-                return this.grey500;
-            case "grey400":
-                return this.grey400;
-            case "grey300":
-                return this.grey300;
-            case "grey200":
-                return this.grey200;
-            case "grey100":
-                return this.grey100;
+            // //Greys
+            // case "black":
+            //     return this.black;
+            // case "white":
+            //     return this.white;
+            // case "grey700":
+            //     return this.grey700;
+            // case "grey600":
+            //     return this.grey600;
+            // case "grey500":
+            //     return this.grey500;
+            // case "grey400":
+            //     return this.grey400;
+            // case "grey300":
+            //     return this.grey300;
+            // case "grey200":
+            //     return this.grey200;
+            // case "grey100":
+            //     return this.grey100;
 
-            //Accents
-            //Green
-            case "green700":
-                return this.green700;
-            case "green600":
-                return this.green600;
-            case "green500":
-                return this.green500;
-            case "green100":
-                return this.green100;
+            // //Accents
+            // //Green
+            // case "green700":
+            //     return this.green700;
+            // case "green600":
+            //     return this.green600;
+            // case "green500":
+            //     return this.green500;
+            // case "green100":
+            //     return this.green100;
 
-            //Orange
-            case "orange700":
-                return this.orange700;
-            case "orange500":
-                return this.orange500;
-            case "orange300":
-                return this.orange300;
-            case "orange100":
-                return this.orange100;
+            // //Orange
+            // case "orange700":
+            //     return this.orange700;
+            // case "orange500":
+            //     return this.orange500;
+            // case "orange300":
+            //     return this.orange300;
+            // case "orange100":
+            //     return this.orange100;
 
-            //Red
-            case "red500":
-                return this.red500;
-            case "red100":
-                return this.red100;
+            // //Red
+            // case "red500":
+            //     return this.red500;
+            // case "red100":
+            //     return this.red100;
 
-            //Other
-            case "purple500":
-                return this.purple500;
-            case "pink500":
-                return this.pink500;
+            // //Other
+            // case "purple500":
+            //     return this.purple500;
+            // case "pink500":
+            //     return this.pink500;
 
             //Role-specific colors
             case "info":
@@ -333,7 +325,7 @@ export const UxpColors = {
             if (gradientTypes.some(type => colorStr.includes(type)))
                 return colorStr;
 
-            //Let's try matching against Fluent tokens first
+            //Let's try matching against Fluent tokens 
             var colorHex = this.getHexFromFluentToken(colorStr);
             if (colorHex)
                 return colorHex;
@@ -343,8 +335,8 @@ export const UxpColors = {
             if (colorHex)
                 return colorHex;
 
-            //Else, let's see if it's already a valid hex value. 
-            //First, remove the hash mark if it has one
+            //Else, let's see if it's a valid hex value. 
+            //Remove the hash mark if it has one
             let h = colorStr.trim().toLowerCase().replace("#", '');
 
             let isHex = false;
