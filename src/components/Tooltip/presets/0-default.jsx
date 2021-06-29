0178a96b5553273edf45ef59d6f26695ff2ca340
Anthony H <ahand@users.noreply.github.com>
2021-06-28 18:42:06 -0700

    Update 0-default.jsx

--- src/components/Tooltip/presets/0-default.jsx
+++ src/components/Tooltip/presets/0-default.jsx
@@ -4,11 +4,9 @@ import Text from '../../Text/Text';
 
 
 
-const instructions = 'Replace this Text control with any Fluent object';
+
 
 
 export default (
-  <Tooltip uxpId="tooltip1" >
-    <Text uxpId="text1" value={instructions} />
-  </Tooltip>
+  <Tooltip uxpId="tooltip1" />
 );
