4cd3b40d2f807bdc0616dd8ed346702d7211d87c
Anthony H <ahand@users.noreply.github.com>
2021-06-28 18:42:59 -0700

    Update 0-default.jsx

--- src/components/Tooltip/presets/0-default.jsx
+++ src/components/Tooltip/presets/0-default.jsx
@@ -5,8 +5,10 @@ import Text from '../../Text/Text';
 
 
 
-
+const instructions = 'Add any Merge control to the CardBody';
 
 export default (
-  <Tooltip uxpId="tooltip1" />
+  <Tooltip uxpId="tooltip1" >
+    <Text uxpId="text2" value={instructions} />
+  </Tooltip>
 );
