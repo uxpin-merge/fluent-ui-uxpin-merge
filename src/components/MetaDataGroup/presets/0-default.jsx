3a8d45e0e64fd7d8ababfae4a3de694e7d59760d
Anthony H <ahand@users.noreply.github.com>
2021-06-28 18:49:43 -0700

    Update 0-default.jsx

--- src/components/MetaDataGroup/presets/0-default.jsx
+++ src/components/MetaDataGroup/presets/0-default.jsx
@@ -9,7 +9,7 @@ const instructions = 'Add any Merge control to the right side of a MetaDataPair.
 export default (
     <MetaDataGroup uxpId="metadatagroup1">
         <MetaDataPair uxpId="metadatapair1" >
-            <Text uxpId="text1" value={instructions} />
+            <Text uxpId="text1" textValue={instructions} />
         </MetaDataPair>
     </MetaDataGroup>
 );
