3bceaed9dbcf5070b482e9915c3853d07b9b4cab
Anthony H <ahand@users.noreply.github.com>
2021-07-08 16:44:47 -0700

    Update GroupButton.js

--- src/components/GroupButton/GroupButton.js
+++ src/components/GroupButton/GroupButton.js
@@ -3,7 +3,6 @@ import * as PropTypes from 'prop-types';
 import Button from '../Button/Button';
 import HorizontalStack from '../HorizontalStack/HorizontalStack';
 import * as UXPinParser from '../_helpers/UXPinParser';
-import { getTokens, csv2arr } from '../_helpers/parser';
 
 
 
@@ -29,16 +28,6 @@ class GroupButton extends React.Component {
 
       var items = [];
 
-      // if (this.props.items) {
-      //    items = csv2arr(this.props.items)
-      //       .flat()
-      //       .map((val, index) => ({
-      //          text: getTokens(val).text,
-      //          key: index + 1,
-      //          icon: this.getLeftIcon(val)
-      //       }));
-      // }
-
       items = UXPinParser.parse(this.props.items).map(
          (item, index) => ({
             key: index + 1,
@@ -57,13 +46,6 @@ class GroupButton extends React.Component {
          items: items,
          selectedIndex: index
       });
-   }
-
-   //Get the user-entered left icon name, if there is one
-   getLeftIcon(str) {
-      const tokens = getTokens(str).tokens
-      const leftIcon = tokens && tokens.find(t => t.type === 'icon' && t.position.placement === 'start')
-      return leftIcon ? leftIcon.target : null
    }
 
    componentDidMount() {
