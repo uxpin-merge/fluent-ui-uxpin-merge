4e231edaaa4eecdfb7caba8f30b43e1e016c81d6
Anthony H <ahand@users.noreply.github.com>
2021-07-16 17:25:44 -0700

    Update 0-default.jsx

--- src/components/ProfileCard/presets/0-default.jsx
+++ src/components/ProfileCard/presets/0-default.jsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
 import ProfileCard from '../ProfileCard';
 import ActionButton from '../../ActionButton/ActionButton';
-import Card from '../../Card/Card';
 
 
 
 export default (
-    <Card uxpId="card1" cardPadding={12}>
-        <ProfileCard uxpId="profilecard1">
-            <ActionButton uxpId="btn1" text="Email" iconName="Mail" />
-            <ActionButton uxpId="btn2" text="Call" iconName="Phone" />
-            <ActionButton uxpId="btn3" text="Chat" iconName="OfficeChat" />
-        </ProfileCard>
-    </Card>
+    <ProfileCard uxpId="profilecard1">
+        <ActionButton uxpId="btn1" text="Email" iconName="Mail" />
+        <ActionButton uxpId="btn2" text="Call" iconName="Phone" />
+        <ActionButton uxpId="btn3" text="Chat" iconName="OfficeChat" />
+    </ProfileCard>
 );
