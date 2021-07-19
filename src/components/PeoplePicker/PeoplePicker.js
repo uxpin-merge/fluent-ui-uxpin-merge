5fa107f968168391d4ebd2ef053be8e57c5fabea
Anthony H <ahand@users.noreply.github.com>
2021-07-19 16:54:56 -0700

    PeoplePicker
    Moving the default people list to the default config file.

--- src/components/PeoplePicker/PeoplePicker.js
+++ src/components/PeoplePicker/PeoplePicker.js
@@ -22,27 +22,6 @@ const textfieldStyle = {
    }
 };
 
-const defaultPeople = `Person1
-Person2
-Person3
-Tiffany Green | tgreen@company.com
-Abhik Rao | Dir., Product Management
-Person6
-Person7
-Person8
-Person9
-Person10
-Person11
-Person12
-Person13
-Person14
-Person15
-Person16
-Person17
-Person18
-Person19
-Person20`;
-
 
 
 class PeoplePicker extends React.Component {
@@ -446,7 +425,7 @@ PeoplePicker.propTypes = {
 
 PeoplePicker.defaultProps = {
    selectedIndexes: '',
-   persons: defaultPeople,
+   persons: '',
    inline: false,
    disabled: false,
 };
