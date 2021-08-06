7435eba5da2eb8093f55b14795ce4f5f6d0a2559
Anthony H <ahand@users.noreply.github.com>
2021-08-05 17:35:15 -0700

    Panel Restoration
    Restoring the Panel control using the native Fluent Panel control.

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,39 +1,99 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import { DefaultButton } from '@fluentui/react/lib/Button';
-import { Panel as PanelM } from '@fluentui/react/lib/Panel';
+import { Panel as FPanel, PanelType } from '@fluentui/react/lib/Panel';
 import { useBoolean } from '@fluentui/react-hooks';
+
+
 
 const Panel = () => {
   const [isOpen, { setTrue: openPanel, setFalse: dismissPanel }] = useBoolean(false);
+}
 
-  return (
-    <div>
-      <DefaultButton text="Open panel" onClick={openPanel} />
-      <PanelM
-        headerText="Sample panel"
-        isOpen={isOpen}
-        onDismiss={dismissPanel}
-        // You MUST provide this prop! Otherwise screen readers will just say "button" with no label.
-        closeButtonAriaLabel="Close"
-      >
-        <p>Content goes here.</p>
-      </PanelM>
-    </div>
-  );
+class Panel extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: true,
+    }
+  }
+
+  componentDidMount() {
+    this.set();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.show !== this.props.show) {
+      this.set();
+    }
+  }
+
+  set() {
+    this.setState({
+      isOpen: this.props.show,
+    });
+  }
+
+  _onDismiss() {
+    this.setState({
+      isOpen: false,
+    });
+
+    if (this.dismiss) {
+      this.dismiss(false);
+    }
+  }
+
+  render() {
+
+    return (
+      <div>
+        <FPanel
+          {...this.props}
+          headerText={this.props.headerText}
+          isOpen={this.state.isOpen}
+          onDismiss={() => this._onDismiss()}
+          closeButtonAriaLabel="Close"
+        >
+          <p>Content goes here.</p>
+        </FPanel>
+      </div >
+    );
+
+  }
+
 };
 
 Panel.propTypes = {
+
   /**
    * @uxpinignoreprop hide this from the user
    */
   children: PropTypes.node,
-  closeButtonAriaLabel: PropTypes.string,
+
+  /**
+   * @uxpindescription To show or hide the panel 
+   */
+  show: PropTypes.bool,
+
+  /**
+  * @uxpindescription The text to be displayed in the header of the panel 
+  * @uxpinpropname Header Text
+  */
+  headerText: PropTypes.string,
+
+  /**
+    * @uxpindescription Fires when the Panel is closed
+    * @uxpinpropname Dismiss
+    */
+  dismiss: PropTypes.func
 };
 
 
 Panel.defaultProps = {
-  closeButtonAriaLabel: 'close'
+
 }
 
 export { Panel as default };
