5dbc47822bc805cc3e6afd699a51364aab6849d5
Anthony H <ahand@users.noreply.github.com>
2021-08-06 17:27:03 -0700

    Text parsing improvements

--- src/components/ActivityItem/ActivityItem.js
+++ src/components/ActivityItem/ActivityItem.js
@@ -8,11 +8,13 @@ import { UxpColors } from '../_helpers/uxpcolorutils';
 import * as UXPinParser from '../_helpers/UXPinParser';
 
 
+
 const defaultIcon = "Info";
-const defaultIconColor = "black";
+const defaultIconColor = "#000000";
 const defaultDescription = 'link(Tahlia) ran a new system test';
 const defaultBody = 'link(System Test #420) contains 3 components. You have been given access privileges.';
 const defaultTimeStamp = 'Just now';
+const linkTarget = 'uxpin_proto_';
 
 
 
@@ -29,8 +31,8 @@ class ActivityItem extends React.Component {
     }
 
     set() {
-        let description = this._getTokenizedText(this.props.description);
-        let comments = this._getTokenizedText(this.props.bodyCopy);
+        let description = this._getMessageText(this.props.description);
+        let comments = this._getMessageText(this.props.bodyCopy);
 
         this.setState(
             {
@@ -53,48 +55,89 @@ class ActivityItem extends React.Component {
         }
     }
 
+    _getMessageText(rawText) {
+        let elements;
+        let parsedOutput = UXPinParser.parse(rawText);
+        // console.log("Text parsedOutput in JSON: " + JSON.stringify(parsedOutput));
+
+        return parsedOutput.map(
+            (item) => {
+                // If not type compound, return the single element
+                if (item.type !== "compound") {
+                    return this._parseItem(item);
+                }
+                else {
+                    // If type compound, map the item values
+                    elements = item.value.map(
+                        (subItem) => {
+                            // Second map of parsedOutput.value to seperate each object of links, icons, and text
+                            return this._parseItem(subItem);
+                        }
+                    )
+                    return elements;
+                }
+            }
+        )
+    }
+
+    _parseItem(item) {
+        if (item) {
+            const key = _.uniqueId('text_');
+            return item.type === "link" ? this._getLinkElement(key, item?.text, item?.href)
+                : this._getTextElement(key, item?.text);
+        }
+    }
+
+    _getTextElement(key, text) {
+        return (<span key={key}> {text} </span>);
+    }
+
+    _getLinkElement(key, text, href) {
+        return (<a key={key} href={href ? href : ''} target={linkTarget}>{text}</a>)
+    }
+
     //Tokenize the string coming in from UXPin for the message 
     //    to support the link(Link Text) feature.
-    _getTokenizedText(text) {
+    // _getTokenizedText(text) {
 
-        // let tokens = UXPinParser.parseRow(text).map(
-        //     (item, index) => {
-        //         if (item.type === "link") {
-        //             console.log("Found a link: (" + item.text + ") href= " + item.href);
-        //             return (
-        //                 // <span>{item.text}</span>
-        //                 <Link
-        //                     value={item.text}
-        //                     linkHref={item.href}
-        //                 />
-        //             );
-        //         }
-        //         if (item.type === "text") {
-        //             return (<span key={index}>{item.text}</span>);
-        //         }
-        //     }
-        // );
+    //     // let tokens = UXPinParser.parseRow(text).map(
+    //     //     (item, index) => {
+    //     //         if (item.type === "link") {
+    //     //             console.log("Found a link: (" + item.text + ") href= " + item.href);
+    //     //             return (
+    //     //                 // <span>{item.text}</span>
+    //     //                 <Link
+    //     //                     value={item.text}
+    //     //                     linkHref={item.href}
+    //     //                 />
+    //     //             );
+    //     //         }
+    //     //         if (item.type === "text") {
+    //     //             return (<span key={index}>{item.text}</span>);
+    //     //         }
+    //     //     }
+    //     // );
 
 
 
-        let tokens = getTokens(text).mixed.map((el, i) => {
-            if (typeof (el) === 'string') {
-                return (<span key={i}> {el} </span>);
-            }
-            else if (el.type == 'link') {
-                return el.suggestions[0];
-            }
-            else if (el.suggestions[0]) {
-                // if there's a suggestion, call the function
-                return el.suggestions[0];
-            } else {
-                // there's no suggestion, return the text
-                return (<span key={i}> {el.tokenString} </span>);
-            }
-        });
+    //     let tokens = getTokens(text).mixed.map((el, i) => {
+    //         if (typeof (el) === 'string') {
+    //             return (<span key={i}> {el} </span>);
+    //         }
+    //         else if (el.type == 'link') {
+    //             return el.suggestions[0];
+    //         }
+    //         else if (el.suggestions[0]) {
+    //             // if there's a suggestion, call the function
+    //             return el.suggestions[0];
+    //         } else {
+    //             // there's no suggestion, return the text
+    //             return (<span key={i}> {el.tokenString} </span>);
+    //         }
+    //     });
 
-        return tokens;
-    }
+    //     return tokens;
+    // }
 
     render() {
 
