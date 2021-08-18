0963edbe4d7d70f011bd9c6c33edb80d1d955586
Anthony H <ahand@users.noreply.github.com>
2021-08-18 15:09:53 -0700

    Update Dropdown.js

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -128,6 +128,7 @@ class Dropdown extends React.Component {
 
     console.log("_onChangeMulti. key: " + option.key);
     console.log("     new index list: " + keys);
+    console.log("     isOpen: " + this?.isOpen);
 
     this.setState(
       {
