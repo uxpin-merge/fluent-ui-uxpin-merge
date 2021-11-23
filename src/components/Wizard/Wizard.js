1d7f4ae587b3b1c0389b8fa84e330be13cff7b2e
Anthony H <ahand@users.noreply.github.com>
2021-11-22 17:38:01 -0800

    Update Wizard.js

--- src/components/Wizard/Wizard.js
+++ src/components/Wizard/Wizard.js
@@ -33,7 +33,7 @@ const stackCenter = 'center';
 
 const headerStackItemStyles = {
     root: {
-        background: "#005a9e", //themeDark
+        background: "#106ebe", //themeDarkAlt
     },
 };
 const wizardStackItemStyles = {
