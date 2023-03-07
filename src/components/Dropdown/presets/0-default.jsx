9fc08d4840792787fde09e8d009b9d307c40e3a9
Anthony H <ahand@users.noreply.github.com>
2023-03-06 17:53:13 -0800

    Dropdown Config Refactor

--- src/components/Dropdown/presets/0-default.jsx
+++ src/components/Dropdown/presets/0-default.jsx
@@ -3,5 +3,22 @@ import Dropdown from '../Dropdown';
 
 
 
+const label = 'Dropdown';
+const placeholder = '- Select -';
+const defaultItems = `Fruit
+* Apples
+* Bananas
+* "I love you, Grapes!"
+divider
+Grains
+Vegetables`;
+
+
+
 export default (
-    <Dropdown uxpId="dropdown1" />);
+    <Dropdown
+        uxpId="dropdown1"
+        label={label}
+        placeholder={placeholder}
+        items={defaultItems}
+    />);
