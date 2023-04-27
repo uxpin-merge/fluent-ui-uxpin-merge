f45553de96ff80e88bec9af8eed7fd5f02506c7e
Evan Trujillo <evantrujillo30@gmail.com>
2023-04-27 12:14:27 +0900

    Add editor config and format new files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { DefaultButton, PrimaryButton } from "@fluentui/react";
-import "./App.css"
+import "./App.css";
 
 export default function App() {
   return (
     <div className="container">
-      <h1>
-        Hello Fluent UI!
-      </h1>
+      <h1>Hello Fluent UI!</h1>
       <div className="btns-group">
-        <DefaultButton text="default button"/>
-        <PrimaryButton text="primary button"/>
+        <DefaultButton text="default button" />
+        <PrimaryButton text="primary button" />
       </div>
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,28 @@
-const path = require('path');
+const path = require("path");
 const config = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: ["./src/index.js"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
+        use: "babel-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   devServer: {
-    'static': {
-      directory: './dist'
-    }
-  }
+    static: {
+      directory: "./dist",
+    },
+  },
 };
 
 module.exports = config;
